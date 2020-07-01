02ca1559641a9135ac2ddbefecfc5c2a849bf35f
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-07-01 15:44:43 +0200

    margin brand

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     </head>
     <body>
     <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand text-white" href="#">OCORDO</a>
+            <a class="navbar-brand text-white ml-3" href="#">OCORDO</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
